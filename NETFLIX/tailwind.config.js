9c67ef58164a33478f67cff3699a792e3a2eb893
Aniket Kumar <aniketkumar202412@gmail.com>
2025-11-13 12:16:01 +0530

    Add files via upload

---
+++ NETFLIX/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}", // Tell Tailwind where your files are
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
